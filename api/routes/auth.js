e6a73bd519a90d3309fbc804c3667003f866750d
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-10-21 12:25:35 -0400

    create user login and register functions

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,42 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("welcome friend to auth route");
+// Register
+router.post("/register", async (req, res) => {
+  try {
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // create new user
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("User not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(401).json("Wrong Password");
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;

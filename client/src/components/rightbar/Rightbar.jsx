f2cf532efcff04353bb983380f5aea757e8f6645
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-10-23 18:06:10 -0400

    create profile page

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -5,10 +5,10 @@ import { CakeOutlined } from "@material-ui/icons";
 import { Users } from "../../testData";
 import Online from "../online/Online";
 
-const Rightbar = () => {
-  return (
-    <div className="rightbar">
-      <div className="rightbarWrapper">
+const Rightbar = ({ profile }) => {
+  const HomeRightBar = () => {
+    return (
+      <>
         <div className="birthdayContainer">
           <CakeOutlined className="birthdayIcon" />
           <span className="birthdayText">
@@ -22,6 +22,51 @@ const Rightbar = () => {
             <Online key={user.id} user={user} />
           ))}
         </ul>
+      </>
+    );
+  };
+
+  const ProfileRightBar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">user info</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">Toronto</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Favorite Pokemon:</span>
+            <span className="rightbarInfoValue">Snorlax</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">user buddies</h4>
+        <div className="rightbarFollowingList">
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/norm.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Jean-Luc</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/norm.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Old Man River</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightBar /> : <HomeRightBar />}
       </div>
     </div>
   );

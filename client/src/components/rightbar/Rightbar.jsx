4ff9b6c176c9414a6ee227acca9078c822c67efb
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-10-23 12:25:04 -0400

    create rightbar component

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 import "./rightbar.css";
+import { CakeOutlined } from "@material-ui/icons";
 
 const Rightbar = () => {
   return (
     <div className="rightbar">
-      <div>rightbar</div>
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <CakeOutlined className="birthdayIcon" />
+          <span className="birthdayText">
+            <b>Jean-Luc</b> and <b>2 other buddies</b> have a birthday today
+          </span>
+        </div>
+        <img src="assets/spicyramen.jpg" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online buddies</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/norm.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jean-Luc</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/norm.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jean-Luc</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };

440f762c45ad86077b7e7e57d2552b8b33d4893b
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-10-24 14:35:02 -0400

    implement context API in login feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 
 import { Home } from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -6,18 +13,18 @@ import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 
 function App() {
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+          {user ? <Home /> : <Register />}
         </Route>
         <Route path="/register">
-          <Register />
+          {user ? <Redirect to="/" /> : <Register />}
         </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
         <Route path="/profile/:username">
           <Profile />
         </Route>
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,14 +1,22 @@
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 import "./login.css";
+import { AuthContext } from "../../context/AuthContext";
+import { loginCall } from "../../apiCalls";
+import { CircularProgress } from "@material-ui/core";
 
 const Login = () => {
   const email = useRef();
   const password = useRef();
 
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("clicked");
+    loginCall(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
   };
+  console.log(user);
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -33,10 +41,20 @@ const Login = () => {
               minLength="6"
               ref={password}
             />
-            <button className="loginButton">Log In</button>
+            <button className="loginButton" type="submit" disabled={isFetching}>
+              {isFetching ? (
+                <CircularProgress color="white" size="20px" />
+              ) : (
+                "Log In"
+              )}
+            </button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
-              Create a New Account
+              {isFetching ? (
+                <CircularProgress color="white" size="20px" />
+              ) : (
+                "Create A New Account"
+              )}
             </button>
           </form>
         </div>
